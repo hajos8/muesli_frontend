058188c8459392c787d4e69e6d78b3e8b3723db5
hajos8 <rabb.andras@gmail.com>
2025-09-23 12:11:19 +0200

    css expand nav

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -8,18 +8,20 @@ import "./App.css"
 
 export default class App extends React.Component{
   state = {
-    menuItemSelected: 'products'
+    menuItemSelected: 'products',
+    menuExpanded: false
   }
 
   handleProductsMenuItemClick = e => this.setState({menuItemSelected: 'products'})
   handleAboutMenuItemClick = e => this.setState({menuItemSelected: 'about'})
-  //handleProductsMenuItemClick = e => this.setState({menuItemSelected: 'products'})
+  
+  handleNavMouse = e => this.setState({menuExpanded: !this.state.menuExpanded})
 
   render(){
     return(
       <div className="page-container">
         
-        <nav id="navbar" title="Toggle menu width" className="expanded">
+        <nav id="navbar" title="Toggle menu width" className={this.state.menuExpanded ? "expanded" : ""} onMouseEnter={this.handleNavMouse} onMouseLeave={this.handleNavMouse}>
           <div className="menu-item" tabIndex="0" data-content="products" onClick={this.handleProductsMenuItemClick}>
             <span className="menu-icon">📦</span>
             <span className="menu-text">Products</span>
